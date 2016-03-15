2fbc02a77e129871bcaa0a6af2654b83f5230ac4
Bartosz Kustra <bart@webflow.com>
2016-03-15 21:43:41 +0100

    fix footer js file

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,5 @@
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
     <script src="//cdn.datatables.net/s/dt/dt-1.10.10,se-1.1.0/datatables.min.js" type="text/javascript"></script>
-    <script type="text/javascript" src="../js/tablica.js"></script>
+    <script type="text/javascript" src="./js/tablica.js"></script>
   </body>
 </html>
