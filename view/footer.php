7c6fac91e870d7fd58896c51270b4847929a3ca1
Bartosz Kustra <bart@webflow.com>
2016-03-15 21:42:48 +0100

    test 2

--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,5 @@
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
     <script src="//cdn.datatables.net/s/dt/dt-1.10.10,se-1.1.0/datatables.min.js" type="text/javascript"></script>
-    <!--<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-    <script src="//cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js" type="text/javascript"></script>-->
-    <script type="text/javascript" src="tablica.js"></script>
+    <script type="text/javascript" src="../js/tablica.js"></script>
   </body>
 </html>
